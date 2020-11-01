ce8e7504f6a7ec13874c0524e2159d38a8e87316
KOUGD7 <cougahdere@yahoo.com>
2020-11-01 18:46:23 -0500

    JavaScript Implementation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+window.addEventListener("load", addmessage);
+
+function addmessage() {
+    const btn = document.querySelector("form .btn");
+    btn.addEventListener("click", function(event) {
+    event.preventDefault();
+    let stat = document.querySelector(".message");
+    
+    msg = document.querySelector("#email").value;
+
+    if (msg == ""){
+        alert("Please enter a valid email address" + msg);
+    }
+    else{
+        stat.textContent = msg;
+        alert("Thank You! Your email address " + msg);
+    }
+    //onsole.log("Nope.");
+    });
+}
